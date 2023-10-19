34b8b2fc724d09cd55b9cd25d38c59ef88046ee4
dedeepnc <dedee.pnc@gmail.com>
2023-10-19 14:28:05 +1100

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const APIKey = "c2bd5a0801msh57cd482c54acc16p1a7d27jsna533972fdcce";
+const APIHost = "netflix-data.p.rapidapi.com";
+const titleID = "80057281";
+const APIURL = `https://${APIHost}/title/trailers/?id=${titleID}`;
+
+window.addEventListener("load", loadData);
+
+async function loadData() {
+    try {
+        const response = await fetch(APIURL, {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': APIKey,
+                'X-RapidAPI-Host': APIHost
+            }
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+        } else {
+            console.error("API request failed.");
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
